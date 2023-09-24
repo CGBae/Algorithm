80f7c6ebb95c255dce2ebe339fdc061fc6254247
CGBae <145093722+CGBae@users.noreply.github.com>
2023-09-24 09:10:55 +0900

    Add files via upload

---
+++ stackL.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stackL.h"
+
+int isStackEmpty() {
+	if (top == NULL) return 1;
+	else return 0;
+}
+
+void push(element item) {
+	stackNode* temp = (stackNode*)malloc(sizeof(stackNode));
+	temp->data = item;
+	temp->link = top;
+	top = temp;
+}
+
+element pop() {
+	element item;
+	stackNode* temp = top;
+
+	if (isStackEmpty()) {
+		printf("\n\n Stack is empty !\n");
+		return 0;
+	}
+	else {
+		item = temp->data;
+		top = temp->link;
+		free(temp);
+		return item;
+	}
+}
